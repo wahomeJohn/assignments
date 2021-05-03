1635e0f14572542f682af1a9cb53f01f249152f8
John Wahome <johnweru206@gmail.com>
2021-05-03 13:43:49 +0300

    final

--- assignment 6.py
+++ assignment 6.py
@@ -1,44 +1,48 @@
-def StudentDetails():
-    studentFname = input("Enter student first  name")
-    studentLname = input("Enter student last  name")
-    studentId        = input("Enter student Id")
-    studentEmail    =input("Enter student Email")
-    studentsex      =input("Enter student sex")
-    StudentFullname = studentFname + " " + studentLname
+#def StudentDetails():
 
-    print("Student Name: " + StudentFullname, "Student Id : " + studentId, "student Email : " + studentEmail,"studentsex :" + studentsex )
-def MailingAddress():
-    address = int(input("Enter address"))
-    city = input("Enter city :")
-    country = input("Enter city :")
-    region = input("Enter region. A for Africa or O for others :")
-    print("Mailing Address :"+ address + " " + city + " " + country + " " + region)
-def CalculateScore():
-    quiz1 = int(input("Enter quiz1 points : "))
-    quiz2 = int(input("Enter quiz2 points  : "))
-    quiz3 = int(input("Enter quiz2 points: "))
-    test1 = int(input("Enter test1 points : "))
-    test2 = int(input("Enter test2 points : "))
-    zoom1 = int(input("Enter zoom1 points : "))
-    zoom2 = int(input("Enter zoom2 points : "))
-    zoom3 = int(input("Enter zoom3 points: "))
-    assignment= int(input("Enter assignment attempted :"))
-    avearagepoints = (quiz1 + quiz2 + quiz3 + test1 +test2)/5
-    zoompoints = zoom1 + zoom2 + zoom3
-    totalpoints = avearagepoints + zoompoints + assignment
-    return totalpoints
+studentFname    = input("Enter student first  name :")
+studentLname    = input("Enter student last  name :")
+studentId       = input("Enter student Id :")
+student_sex     = input("Enter student sex :")
+studentEmail    = studentFname + studentLname + "@gmail"
+studentFullname = studentFname + " " + studentLname
+
+#def MailingAddress():
+
+address = input("Enter address:")
+city = input("Enter city :")
+country = input("Enter country :")
+region = input("Enter region. A for Africa or O for others :")
+
+
+#def CalculateScore():
+
+quiz1 = int(input("Enter quiz1 points : "))
+quiz2 = int(input("Enter quiz2 points : "))
+quiz3 = int(input("Enter quiz2 points : "))
+test1 = int(input("Enter test1 points : "))
+test2 = int(input("Enter test2 points : "))
+zoom1 = int(input("Enter zoom1 points : "))
+zoom2 = int(input("Enter zoom2 points : "))
+zoom3 = int(input("Enter zoom3 points : "))
+assignment= int(input("Enter assignment attempted :"))
+assignment_percent = (assignment/10)* 100
+avearagepoints = (quiz1 + quiz2 + quiz3 + test1 +test2)/5
+zoompoints = ((zoom1 + zoom2 + zoom3)/9)* 100
+totalpoints = (avearagepoints + zoompoints + assignment_percent)/3
+   # return totalpoints
+
+print("Student Name: " + studentFullname, "Student Id : " + studentId, "student Email : " + studentEmail,"studentsex :" + student_sex )
+print("Mailing Address :"+ address + " " + city + " " + country + " " + region)
 
 while region == "A":
-    if student_sex == male:
-        if totalpoints >= 80:
-            print("Scholarship Awarded")
-        else:
-            print("No Scholarship")
-    elif student_sex == female:
-        if totalpoints >= 76:
-            print("Scholarship Awarded")
-        else:
-            print("No Scholarship")
+#def conditional():
+    if student_sex == "male" and totalpoints >= 80:
+           # if totalpoints >= 80:
+        print(" Full Scholarship Awarded")
+    elif student_sex == "female" and totalpoints >= 76:
+        print("Full Scholarship Awarded")
+    else:
+        print("No Scholarship")
 
-StudentDetails()
-MailingAddress()
+    break
